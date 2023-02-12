b7816ca08a9d1a788fd114b125dc783d8b2b1b75
Edward A Maxedon Sr <edadma@gmail.com>
2023-02-12 17:36:05 -0500

    pdf

--- src/main/scala/io/github/edadma/compositor/Compositor.scala
+++ src/main/scala/io/github/edadma/compositor/Compositor.scala
@@ -1,5 +1,19 @@
 package io.github.edadma.compositor
 
-import io.github.edadma.libcairo.{TextExtents}
+import io.github.edadma.libcairo.{Context, Surface, TextExtents, pdfSurfaceCreate}
 
-class Compositor
+import scala.collection.mutable.ArrayBuffer
+
+class Compositor:
+  val surfaces = new ArrayBuffer[Surface]
+  val contexts = new ArrayBuffer[Context]
+
+  def pdf(path: String, width: Double, height: Double): Context =
+    val surface = pdfSurfaceCreate(path, width, height)
+
+    surfaces += surface
+
+    val context = surface.create
+
+    contexts += context
+    context
