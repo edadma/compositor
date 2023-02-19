c4be3237cb342e5565fb27a5f4cc35580a9d8a38
Edward Adrian Maxedon <edadma@gmail.com>
2023-02-19 16:52:45 -0500

    Compositor

--- src/main/scala/io/github/edadma/compositor/Box.scala
+++ src/main/scala/io/github/edadma/compositor/Box.scala
@@ -4,6 +4,7 @@ import io.github.edadma.libcairo.Context
 
 abstract class Box:
   def height: Double
+  def ascent: Double
   def descent: Double
   def width: Double
   def draw(ctx: Context, x: Double, y: Double): Unit
