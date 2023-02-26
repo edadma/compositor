4574a70a9360b0d4c373b3cb71605654d1c37199
Edward A Maxedon Sr <edadma@gmail.com>
2023-02-25 21:00:12 -0500

    lowerThirdPage

--- src/main/scala/io/github/edadma/compositor/LowerThirdPage.scala
+++ src/main/scala/io/github/edadma/compositor/LowerThirdPage.scala
@@ -7,17 +7,14 @@ class LowerThirdPage(comp: Compositor, val lineWidth: Double, pageHeight: Double
 
   override def draw(comp: Compositor, x: Double, y: Double): Unit =
     comp.color(0, 0, 0, 0)
-    comp.ctx.moveTo(x, y)
     comp.ctx.rectangle(x, y, lineWidth, 2.0 / 3 * pageHeight)
     comp.ctx.fill()
     comp.color(0, 0, 1)
-    comp.ctx.moveTo(x, y + 2.0 / 3 * pageHeight)
-    comp.ctx.rectangle(x, y, lineWidth, 1.0 / 3 * pageHeight)
+    comp.ctx.rectangle(x, y + 2.0 / 3 * pageHeight, lineWidth, 1.0 / 3 * pageHeight)
     comp.ctx.fill()
     super.draw(comp, x + 10, y + 2.0 / 3 * pageHeight + 10)
     comp.ctx.setLineWidth(.5)
     comp.color(1, 0, 0)
-    comp.ctx.moveTo(x, y)
     comp.ctx.rectangle(x, y, lineWidth, pageHeight)
     comp.ctx.stroke()
 
