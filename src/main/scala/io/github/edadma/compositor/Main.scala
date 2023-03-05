b10be7ab374912345416a2c19b148b9a20d32c6c
Edward A Maxedon Sr <edadma@gmail.com>
2023-03-05 18:58:40 -0500

    main

--- src/main/scala/io/github/edadma/compositor/Main.scala
+++ src/main/scala/io/github/edadma/compositor/Main.scala
@@ -12,7 +12,7 @@ import io.github.edadma.libcairo.pdfSurfaceCreate
 
   doc.color("white")
   doc add new FrameBox(new RigidBox(doc.pageWidth, doc.pageHeight * 2 / 3)) { background = Color.TRANSPARENT }
-  doc.startPage(new SimplePage(doc.pageWidth - 10, Some(doc.pageHeight * 3)))
+  doc.startPage(new SimplePage(doc.pageWidth - 10, Some(doc.pageHeight * 3 - 5)))
   verses(
     doc,
     "20 But as he considered these things, behold, an angel of the Lord appeared to him in a dream, saying, “Joseph, son of David, do not fear to take Mary as your wife, for that which is conceived in her is from the Holy Spirit. 21 She will bear a son, and you shall call his name Jesus, for he will save his people from their sins.” 22 All this took place to fulfill what the Lord had spoken by the prophet:\n\n23 “Behold, the virgin shall conceive and bear a son,\n    and they shall call his name Immanuel”\n\n(which means, God with us).",
