aaf6524590e89a13b485e1860b06044118282fec
Edward A Maxedon Sr <edadma@gmail.com>
2023-03-05 16:42:55 -0500

    verses

--- src/main/scala/io/github/edadma/compositor/package.scala
+++ src/main/scala/io/github/edadma/compositor/package.scala
@@ -5,3 +5,25 @@ import math.sqrt
 def ppi(width: Int, height: Int, diagonal: Double): Double = sqrt(width * width + height * height) / diagonal
 
 type PageFactory = (Compositor, Double, Double) => PageBox
+
+private val numberRegex = "[0-9]+".r
+private val footnoteRegex = "[^[]+\\[[a-z]]".r
+
+def verses(comp: Compositor, text: String): Unit =
+  val words = text.split("\\s+")
+  var i = 0
+
+  while i < words.length do
+    val word = words(i)
+
+    if numberRegex.matches(word) && i < words.length - 1 then
+      comp.prefixSup(word, words(i + 1))
+      i += 2
+    else
+      if footnoteRegex.matches(word) then comp.addWord(word.dropRight(3))
+      else comp.addWord(word)
+
+      i += 1
+
+  comp.paragraph()
+end verses
