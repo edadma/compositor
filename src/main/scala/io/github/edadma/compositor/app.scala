1bc012514f1e96bc3e905bce246fb8b3713b9e89
Edward Adrian Maxedon <edadma@gmail.com>
2023-03-12 18:08:58 -0400

    app

--- src/main/scala/io/github/edadma/compositor/app.scala
+++ src/main/scala/io/github/edadma/compositor/app.scala
@@ -1,5 +1,6 @@
 package io.github.edadma.compositor
 
+import java.io.FileOutputStream
 import scala.util.Using
 import java.nio.file.Files
 
@@ -10,12 +11,19 @@ def app(config: Config): Unit =
       case Config(Some(file), _, _, _, _, _) =>
         val path = file.toPath.normalize.toAbsolutePath
 
-        if !Files.exists(path) then problem(s"file '$path' not found")
-        else if !Files.isReadable(path) then problem(s"file '$path' is unreadable")
-        else if !Files.isRegularFile(path) then problem(s"file '$path' is not a file")
+        if !Files.exists(path) then problem(s"'$path' not found")
+        else if !Files.isReadable(path) then problem(s"'$path' is not readable")
+        else if !Files.isRegularFile(path) then problem(s"'$path' is not a file")
         else new String(Files.readAllBytes(file.toPath))
+  val output =
+    config match
+      case Config(_, None, _, _, _, _) => Console.out
+      case Config(_, Some(file), _, _, _, _) =>
+        val path = file.toPath.normalize.toAbsolutePath
 
-  println(input)
+        if !Files.isWritable(path) then problem(s"'$path' is not writable")
+        else new FileOutputStream(file)
+
 //      case Config(input, output, "pdf", paper, _, _) => null
 //      case Config(input, output, "png", _, resolution, size) =>
 //        println(input.get.toPath) // Using(scala.io.Source.fromFile(".gitignore"))(_.mkString).get
