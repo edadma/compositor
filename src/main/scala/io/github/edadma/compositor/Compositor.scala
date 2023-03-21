954e1e2c1cd950eeb306f1611e430fb5e36410ee
Edward A Maxedon Sr <edadma@gmail.com>
2023-03-20 22:23:25 -0400

    commands

--- src/main/scala/io/github/edadma/compositor/Compositor.scala
+++ src/main/scala/io/github/edadma/compositor/Compositor.scala
@@ -314,8 +314,6 @@ abstract class Compositor private[compositor]:
       case _                =>
 
   def output(): Unit =
-    paragraph()
-
     while modeStack.nonEmpty do modeStack.pop.done()
 
     for (p, i) <- pages.zipWithIndex do
