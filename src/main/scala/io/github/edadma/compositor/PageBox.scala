1ecfc3d5881607316b8eefba2472d1d43d1cba67
Edward Adrian Maxedon <edadma@gmail.com>
2023-03-25 16:55:50 -0400

    AbstractBox

--- src/main/scala/io/github/edadma/compositor/PageBox.scala
+++ src/main/scala/io/github/edadma/compositor/PageBox.scala
@@ -1,8 +1,4 @@
 package io.github.edadma.compositor
 
-import scala.collection.mutable.ArrayBuffer
-
 trait PageBox extends AddableBox:
   def lineWidth: Double
-  def add(box: Box): AddableBox
-  def set(): Unit
--- src/main/scala/io/github/edadma/compositor/SimplePage.scala
+++ src/main/scala/io/github/edadma/compositor/SimplePage.scala
@@ -1,17 +1,8 @@
 package io.github.edadma.compositor
 
 class SimplePage(val lineWidth: Double, pageHeight: Option[Double] = None) extends VTop with PageBox:
-  def set(): Unit =
-    setToWidth(lineWidth)
-    pageHeight foreach setToHeight
-
-  override def height: Double =
-    println((pageHeight, super.height))
-    pageHeight.getOrElse(super.height) // todo: seems hacky; setToHeight isn't called if inside a FrameBox
-
-  override def setToWidth(width: Double): Unit = super.setToWidth(lineWidth)
-
-  override def setToHeight(height: Double): Unit = pageHeight foreach super.setToHeight
+  setToWidth(lineWidth)
+  pageHeight foreach setToHeight
 
 def simplePageFactory(fixedHeight: Boolean = true): PageFactory =
   (comp: Compositor, width: Double, height: Double) => new SimplePage(width, Option.when(fixedHeight)(height))
