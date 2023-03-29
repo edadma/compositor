e0413e41cbfe3b1f222a9f96b3aae3e242cc5221
Edward A Maxedon Sr <edadma@gmail.com>
2023-03-29 07:20:03 -0400

    multi

--- src/main/scala/io/github/edadma/compositor/app.scala
+++ src/main/scala/io/github/edadma/compositor/app.scala
@@ -13,67 +13,70 @@ import pprint._
 def app(args: Config): Unit =
   val input =
     args match
-      case Config(None, _, _, _, _, _) => scala.io.Source.stdin.mkString
-      case Config(Some(file), _, _, _, _, _) =>
+      case Config(None, _, _, _, _, _, _) => scala.io.Source.stdin.mkString
+      case Config(Some(file), _, _, _, _, _, _) =>
         val path = file.toPath.normalize.toAbsolutePath
 
         if !Files.exists(path) then problem(s"'$path' not found")
         else if !Files.isReadable(path) then problem(s"'$path' is not readable")
         else if !Files.isRegularFile(path) then problem(s"'$path' is not a file")
         else new String(Files.readAllBytes(file.toPath))
-  val output =
-    args match
-      case Config(None, None, typ, _, _, _) => Paths.get(s"$out.$typ").normalize.toAbsolutePath
-      case Config(Some(file), None, typ, _, _, _) =>
-        val path = file.toPath.normalize.toAbsolutePath
 
-        path.getParent.resolve(s"${path.getFileName.toString}.$typ")
-      case Config(_, Some(file), _, _, _, _) => file.toPath.normalize.toAbsolutePath
+  def process(in: String, config: Config, n: Int): Unit =
+    val output =
+      args match
+        case Config(None, None, typ, _, _, _) => Paths.get(s"$out.$typ").normalize.toAbsolutePath
+        case Config(Some(file), None, typ, _, _, _) =>
+          val path = file.toPath.normalize.toAbsolutePath
 
-  if !Files.isWritable(output.getParent) then problem(s"'$output' is not writable")
+          path.getParent.resolve(s"${path.getFileName.toString}.$typ")
+        case Config(_, Some(file), _, _, _, _) => file.toPath.normalize.toAbsolutePath
 
-  val doc =
-    args match
-      case Config(_, _, "pdf", paper, _, _, _) =>
-        val p =
-          paper match
-            case "a4"     => Paper.A4
-            case "letter" => Paper.LETTER
+    if !Files.isWritable(output.getParent) then problem(s"'$output' is not writable")
 
-        Compositor.pdf(output.toString, p, simplePageFactory())
-      case Config(_, _, "png", _, resolution, size, _) =>
-        val (width, height) =
-          resolution match
-            case "sd"  => (720, 480)
-            case "hd"  => (1280, 720)
-            case "fhd" => (1920, 1080)
+    val doc =
+      args match
+        case Config(_, _, "pdf", paper, _, _, _) =>
+          val p =
+            paper match
+              case "a4"     => Paper.A4
+              case "letter" => Paper.LETTER
 
-        Compositor.png(output.toString, width, height, ppi(width, height, size), simplePageFactory())
-      case _ => sys.error("error")
-  val config =
-    Map(
-      "today" -> "MMMM d, y",
-      "include" -> ".",
-      "rounding" -> "HALF_EVEN",
-    )
-  val parser = new Parser(Command.builtins ++ commands, Nil, blanks = true)
-  var newlineCount: Int = 0
-  val out: PartialFunction[Any, Unit] = {
-    case "\n" if newlineCount == 0 => newlineCount += 1
-    case "\n" =>
-      doc.paragraph()
-      newlineCount = 0
-    case s: String if s.isBlank =>
-    case s: String =>
-      doc.add(s)
-      newlineCount = 0
-  }
-  val renderer = new Renderer(parser, config, _.mkString, doc, out)
-  val ast = parser.parse(input)
+          Compositor.pdf(output.toString, p, simplePageFactory())
+        case Config(_, _, "png", _, resolution, size, _) =>
+          val (width, height) =
+            resolution match
+              case "sd"  => (720, 480)
+              case "hd"  => (1280, 720)
+              case "fhd" => (1920, 1080)
 
-  // pprintln(ast)
-  println(s"page width = ${doc.pageWidth}")
-  println(s"page height = ${doc.pageHeight}")
-  renderer.render(ast)
-  doc.output()
-  doc.destroy()
+          Compositor.png(output.toString, width, height, ppi(width, height, size), simplePageFactory())
+        case _ => sys.error("error")
+    val config =
+      Map(
+        "today" -> "MMMM d, y",
+        "include" -> ".",
+        "rounding" -> "HALF_EVEN",
+      )
+    val parser = new Parser(Command.builtins ++ commands, Nil, blanks = true)
+    var newlineCount: Int = 0
+    val out: PartialFunction[Any, Unit] = {
+      case "\n" if newlineCount == 0 => newlineCount += 1
+      case "\n" =>
+        doc.paragraph()
+        newlineCount = 0
+      case s: String if s.isBlank =>
+      case s: String =>
+        doc.add(s)
+        newlineCount = 0
+    }
+    val renderer = new Renderer(parser, config, _.mkString, doc, out)
+    val ast = parser.parse(input)
+
+    // pprintln(ast)
+    println(s"page width = ${doc.pageWidth}")
+    println(s"page height = ${doc.pageHeight}")
+    renderer.render(ast)
+    doc.output()
+    doc.destroy()
+  end process
