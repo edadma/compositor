d1dd19b21bffbcf86fc8ff99449eed96a3684911
Edward Adrian Maxedon <edadma@gmail.com>
2023-04-03 17:46:35 -0400

    ImageBox

---
+++ src/main/scala/io/github/edadma/compositor/ImageBox.scala
@@ -0,0 +1,8 @@
+package io.github.edadma.compositor
+
+class ImageBox(path: String, width: Option[Double] = None, height: Option[Double] = None) extends SimpleBox:
+
+  def ascent: Double = ???
+  def draw(comp: io.github.edadma.compositor.Compositor, x: Double, y: Double): Unit = ???
+  def typ: io.github.edadma.compositor.Type = ???
+  def width: Double = ???
