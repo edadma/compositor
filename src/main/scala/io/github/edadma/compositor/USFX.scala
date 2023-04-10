71a33d0a8497da8e514b8afb8c6715354e358750
Edward Adrian Maxedon <edadma@gmail.com>
2023-04-10 12:52:54 -0400

    usfx

--- src/main/scala/io/github/edadma/compositor/USFX.scala
+++ src/main/scala/io/github/edadma/compositor/USFX.scala
@@ -5,17 +5,23 @@ import scala.xml.{XML, Node, Elem, Text}
 object USFX:
   def fromXML(comp: Compositor, node: Node): Unit =
     node match
+      case Elem(prefix, "id" | "ide" | "ve" | "f" | "ft" | "fr", attribs, scope, child @ _*) =>
       case Elem(prefix, label, attribs, scope, child @ _*) =>
         def body(): Unit = child foreach (c => fromXML(comp, c))
 
         (label, attribs.asAttrMap.headOption) match
+          case ("book", _) => body()
+          case ("h", _)    => body()
           case ("toc", "level" -> "1") =>
             comp.bold()
             body()
             comp.nobold()
+          case ("toc", _)            =>
+          case ("wj", _)             => body()
+          case ("p", "sfm" -> "mt")  =>
+          case ("p", "style" -> "p") => body()
           case _ => sys.error(s"don't know what to do with element <$label> with attributes $attribs")
-
-      case Text(text) => if !text.isBlank then comp.add(text)
+      case Text(text) => if !text.isBlank then comp.add(text.trim)
       case _          => sys.error(s"don't know what to do with '$node' (${node.getClass})")
 
   def fromString(comp: Compositor, s: String): Unit = fromXML(comp, XML.loadString(s))
