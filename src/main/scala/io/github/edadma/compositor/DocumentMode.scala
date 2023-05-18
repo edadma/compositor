340125673b083bdab87221123c6042cb20c6608c
Edward Adrian Maxedon <edadma@gmail.com>
2023-05-18 07:58:29 -0400

    a vfill should be added at the end of the last page

--- src/main/scala/io/github/edadma/compositor/DocumentMode.scala
+++ src/main/scala/io/github/edadma/compositor/DocumentMode.scala
@@ -5,6 +5,8 @@ import scala.collection.mutable.ArrayBuffer
 class DocumentMode(protected val comp: Compositor) extends Mode:
   def add(box: Box): Unit = comp.pages += box.asInstanceOf[PageBox]
 
-  override def done(): Unit = pop
+  override def done(): Unit =
+    comp.pages.last.add(new VSpaceBox(2, 0, 1))
+    pop
 
   def result: Box = ???
